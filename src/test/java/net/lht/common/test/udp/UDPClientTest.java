aac59fe1f0834d26bdaabb506c101dafe44ae731
刘洪天 <liuht@bhz.com.cn>
2018-12-26 14:55:23 +0800

    temp remarked

--- src/test/java/net/lht/common/test/udp/UDPClientTest.java
+++ src/test/java/net/lht/common/test/udp/UDPClientTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executors;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+//import org.junit.Test;
 
 import net.lht.common.udp.UDPClient;
 
@@ -22,26 +22,27 @@ public class UDPClientTest {
 	public void tearDown() throws Exception {
 	}
 
-	//@Test
+	// @Test
 	public void test() {
 		int poolSize = 100;
 		ExecutorService pool = Executors.newFixedThreadPool(poolSize);
 		Random r = new Random();
-		for(int i = 0; i < poolSize; i++) {
+		for (int i = 0; i < poolSize; i++) {
 			System.out.println("submit");
 			pool.submit(() -> {
-				for(int j = 0; j < 500; j++) {
-//					try {
-//						Thread.sleep(Math.abs(r.nextInt() % 2000));
-//					} catch (InterruptedException e) {
-//						e.printStackTrace();
-//					}
+				for (int j = 0; j < 500; j++) {
+					try {
+						Thread.sleep(Math.abs(r.nextInt() % 2000));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 					byte[] ack = UDPClient.sendAndReceive("127.0.0.1", 5002, "SendStringliuhongtian".getBytes());
 					System.out.println(j + " Server ACK: " + new String(ack));
 				}
 			});
 		}
 		pool.shutdown();
+		assertTrue(true);
 	}
-	
+
 }
